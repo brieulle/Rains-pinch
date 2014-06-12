7ad6fc2aa51ff7f58fc6347667dc9004a03ad644
brieulle <brieulle@users.noreply.github.com>
2014-06-13 00:24:57 +0200

    Update accept.py

--- accept.py
+++ accept.py
@@ -21,60 +21,47 @@ def find_trace(n,m,k):
     # If m is a multiple of p, then we just need the trace to be of order 
     #exactly n in (Z/m)*
     if m%p == 0:
-        count = 0
-        t = ZZ(1)
-        while true:
-            if count >= euler_phi(m):
-                raise RuntimeError, 'No suitable trace found.'
+        sol = []
+        for t in Zm
             # We only want the trace to be of order exactly n in (Z/m)* and not 
             # to define supersingular curves.
-            if t.gcd(m) != 1:
-                count +=1
+            if not t.is_unit():
                 continue
             elif (Zm(t).multiplicative_order() != n):
-                t += 1
-                count +=1
+                continue
             else:
-                return t 
+                sol.append(t)
+        return sol
     # If m is prime (power), then we need to look at the roots
     # Probably a temporary condition
     elif m.is_prime_power():
         #TODO : rÃ©flÃ©chir Ã  la forme que a pourrait Ã©ventuellement avoir..
-        count = 0
         sol = []
-        while true:
-            for a in Zm:
-                if count >= euler_phi(m):
-                    return list(set(sol)) #raise RuntimeError, 'Did not found any workable trace'
-                # If a is not invertible in Z/m, we can't compute any order.
-                # We'll probably have to look for an element of order exactlu n
-                # We don't want a = 0 mod m. 
-                if not a.is_unit():
-                    count += 1
+        for a in Zm:
+            # If a is not invertible in Z/m, we can't compute any order.
+            # We'll probably have to look for an element of order exactlu n
+            # We don't want a = 0 mod m. 
+            if not a.is_unit():
+                continue
+            else:
+                ord_a = Zm(a).multiplicative_order()
+                ord_b = Zm(q/a).multiplicative_order()
+                # We need an element of order n
+                if (ord_a != n and ord_b !=n):
                     continue
-                else:
-                    ord_a = Zm(a).multiplicative_order()
-                    ord_b = Zm(q/a).multiplicative_order()
-                    # We need an element of order n
-                    if (ord_a != n and ord_b !=n):
-                        count +=1
-                        continue
-                    # We want ord_a != ord_b
-                    elif (ord_a == ord_b):
-                        count +=1
-                        continue
-                    elif (ord_b != n): 
-                        if (ord_a > ord_b):
-                            count += 1
-                            continue
-                        else:
-                            count += 1
-                            sol.append((a, a + q/a)) #return (a, a + q/a, q, q/a)
-                    elif (ord_b > ord_a):
-                        count += 1
+                # We want ord_a != ord_b
+                elif (ord_a == ord_b):
+                    continue
+                elif (ord_b != n): 
+                    if (ord_a > ord_b):
                         continue
                     else:
-                        count += 1
-                        sol.append((q/a, a + q/a)) #return (q/a, a + q/a)
+                        sol.append((a, a + q/a)) #return (a, a + q/a, q, q/a)
+                elif (ord_b > ord_a):
+                    continue
+                else:
+                    sol.append((q/a, a + q/a)) #return (q/a, a + q/a)
+        return sol
+        
     else:
         raise NotImplementedError, 'm composite is not implemented yet'
