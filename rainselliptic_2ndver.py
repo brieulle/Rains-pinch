2d15bbf4a372277efaff96cf4b9c9cdfc820c343
brieulle <brieulle@users.noreply.github.com>
2014-06-24 17:46:33 +0200

    Update rainselliptic_2ndver.py

--- rainselliptic_2ndver.py
+++ rainselliptic_2ndver.py
@@ -3,57 +3,62 @@
 def isom_elliptic(k1, k2, k = None, Y_coordinates = False, bound = None):
     '''
     INPUT : 
-    a base field k, 
-    two extensions of the same degree k1, k2,
-    a boolean Y_coordinates. 
-
-    OUTPUT : a tuple with normal elements in k1 and k2 respectively.
-
-    Use :
-
-    isom_elliptic(k1, k2, k)
-
-    returns two normal elements using abscissas of torsion point. If you want to
-    use the ordinates, write 
     
-    isom_elliptic(k, k1, k2, True)
+    - ``k1`` -- a finite field, extension of degree n of k.
 
-    Algorithm:
+    - ``k2`` -- a finite field, extension of degree n of k; k1 != k2.
+
+    - ``k`` -- (default : None) a  finite field of characteristic p!=2, it plays
+      the role of the base field for k1 & k2.
+
+    - ``Y_coordinates`` -- (default : False) a boolean that will be used to 
+      know whether to use Y or X coordinates for the Gaussian elliptic periods.
+
+    - ``bound`` -- (default : None) a positive integer used as the max for m.
+
+    OUTPUT : 
+    
+    - A tuple of unique elements with the same minimal polynomial in k1 and k2 
+      respectively
+
+    EXAMPLES :
+
+        sage : R.<X> = PolynomialRing(GF(5))
+
+        sage : f = X^19 + X^16 + 3*X^15 + 4*X^14 + 3*X^12 + 3*X^9 + 2*X^8 + 2*X^7 + 2*X^4 + X^3 + 4*X^2 + 4*X + 2)
+        
+        sage : g = X^19 + 2*X^18 + 2*X^17 + 4*X^16 + X^15 + 3*X^14 + 2*X^13 + X^12 + 2*X^11 + 2*X^10 + 2*X^9 + X^8 + 4*X^6 + X^5 + 3*X^4 + 2*X^2 + 4*X + 4
+
+        sage : k1 = GF(5**19, name = 'x', modulus = f)
+
+        sage : k2 = GF(5**19, name = 'y', modulus = g)
+
+        sage : tuple = isom_elliptic(k1, k2)
+
+        sage : tuple[0].minpoly() == tuple[1].minpoly()
+
+        True
+
+        sage : tuple_Y = isom_elliptic(k1, k2, Y_coordinates = True)
+
+        sage : tuple_Y[0].minpoly() == tuple_Y[1].minpoly()
+
+        True
+
+    ALGORITHM:
 
     Given two extensions of the same base field and the same degree, we return 
-    two normal elements the use of Gaussian elliptic period, via the function 
-    find_unique_orbit_elliptic, on an curve E which is determined by the 
-    function find_elliptic_curve.
+    two unique elements the use of Gaussian elliptic period on normal elements,
+    via the function find_unique_orbit_elliptic, on an curve E which is 
+    determined by the function find_elliptic_curve.
 
     First we have to find an integer m and an elliptic curve E  over k such 
     that :
+    TODO 
 
-    - m divides the number of points of E over the extensions k1 and k2.
+    .. TODO::
 
-    - if m is power of p, we want that the trace t of the Frobenius on E over k 
-      is of order n, the degree of the extension in (Z/m)*.
-
-    - if m is a prime power, we want that the root of smallest order in (Z/m)*
-      of the characteristic polynomial of the Frobenius over k is of order n,
-      the degree of the extension, i.e.:
-
-      For x**2 -tx + q = (x - a)(x - b) mod m, we want that ord_m(a) = n nd
-      ord_m(a) < ord_m(b).
-
-    - if m is composite, there is no algorithm yet.
-
-    Once we have both of them, we have to compute two m torsion points P and Q 
-    of order m in E/k1 and E/k2 respectively and then we compute their Gaussian
-    elliptic periods with abscissas or ordinates depending on the boolean given 
-    in arguments; that is done in find_unique_orbit_elliptic.
-    
-    Thus we found two unique elements alpha and beta such that there's exist an 
-    isomorphism phi with :
-
-    phi(alpha) = beta
-
-    and you can find phi from there using the function convert as done in the 
-    function isom_normal.
+        The case j = 1728 and j = 0.
     '''
     if k is None:
 	    k = k1.base_ring()
@@ -63,70 +68,68 @@ def isom_elliptic(k1, k2, k = None, Y_coordinates = False, bound = None):
     
     # We compute a list of candidates for m (i.e. such that n divides phi(m) 
     # and (phi(m)/n,n) = 1. It lacks the conditions on the trace.
-    #c_comp, w_comp = cputime(), walltime()
-    #c, w = cputime(), walltime()
     m_t = find_m(n, k, bound)
-    #print 'Computing the m\'s : CPU %s, Walltime %s' % (cputime(c), walltime(w))
     
     if m_t is None:
         raise RuntimeError, 'No suitable m found, increase your bound.'
 
     # Finding the elliptic curve on which we can work. 
-    #c, w = cputime(), walltime()
     E, case, m = find_elliptic_curve(k, k1, m_t) 
-    #print 'Finding E : CPU %s, Walltime %s' % (cputime(c), walltime(w))
 
     if E is None:
         raise RuntimeError, 'No suitable elliptic curve found, check your \
                                                                     parameters'
 
-    #c, w = cputime(), walltime()
     Ek1 = E.change_ring(k1)
     Ek2 = E.change_ring(k2)
-    #print 'Computing E in extension : CPU %s, Walltime %s' % (cputime(c),
-    #                                                             walltime(w))
 
-    #c, w = cputime(), walltime()
     a, b = (find_unique_orbit_elliptic(Ek1, m, Y_coordinates, case),
     find_unique_orbit_elliptic(Ek2, m, Y_coordinates, case))
-    #print 'Computing periods : CPU %s, Walltime %s' % (cputime(c), walltime(w))
-    #print 'Total time : CPU %s, Walltime %s' % (cputime(c_comp),
-    #                                                         walltime(w_comp))
-    #if not a or not b:
-    #    return m_t, E
 
     return a, b
 
-def find_unique_orbit_elliptic(E, m, Y_coordinates = False, case = None):
+def find_unique_orbit_elliptic(E, m, Y_coordinates = False, case = 0):
     '''
     INPUT : 
     
-    an elliptic curve E, an integer m, a boolean Y_coordinates
+    - ``E`` -- an elliptic curve with the properties given in isom_elliptic 
+      and/or find_elliptic_curve.
+
+    - ``m`` -- an integer with the properties given in isom_elliptic and/or in 
+      find_m.
+
+    - ``Y_coordinates`` -- boolean (default : False) determines if X or Y 
+      coordinates are used.
+
+    - ``case`` -- integer (default : 0) depends on the j-invariant's value :
+        - ``0`` means j is neither 0 or 1728,
+        - ``1`` means j is 1728,
+        - ``2`` means j is 0.
 
     OUPUT : 
     
-    a Gaussian elliptic periods of point P of order m in E/K, K a finite field
+    - A uniquely defined element of the field where E is defined, namely the 
+      extension of degree n considered; unique means every produced elements 
+      have the same minimal polynomial.
 
-    Use : 
-    find_unique_orbit_elliptic(E, m)
+    EXAMPLES :
 
-    returns a Gaussian elliptic periods using the X-coordinates of a point P of 
-    order m. If you want to use the Y-coordinates, type :
+    sage: E = EllipticCurve(j = GF(5)(1))
 
-    find_unique_orbit_elliptic(E, m, True)
+    sage: EK = E.change_ring(GF(5**19, prefix = 'z', conway = True)
 
-    case = None => default case : j != 0, 1728 or t = 0
-    case = 1 => j = 1728 and t != 0
-    case = 2 => j = 0 and t != 0
+    sage: m = 229
 
-    Algorithm:
+    sage: elem1 = find_unique_orbit_elliptic(EK,m)
 
-    Function that given an elliptic curve and a group G returns the following 
-    Gaussian elliptic periods :
+    sage: elem2 = find_unique_orbit_elliptic(EK,m)
 
-    sum_{\sigma\in G}{([\sigma]P)_{T}}
+    sage: elem1.minpoly() == elem2.minpoly()
 
-    with T = X or Y depending on the boolean given in arguments.
+    True
+
+    ALGORITHM:
+    TODO
     '''
     cofactor = E.cardinality()//m
     n = E.base_ring().degree()
@@ -136,7 +139,7 @@ def find_unique_orbit_elliptic(E, m, Y_coordinates = False, case = None):
     while any((m//i)*P == 0 for i in m.prime_divisors()):
         P = cofactor*E.random_point()
 
-	if case is None:
+    if case == 0:
         # Looking for a generator of order exactly phi(m)/n in 
         # phi(m)/something.
         gen_G = Zmod(m).unit_gens()[0]**n
@@ -155,29 +158,66 @@ def find_unique_orbit_elliptic(E, m, Y_coordinates = False, case = None):
         order = euler_phi(m)//(4*n)
         
         if not Y_coordinates:
-            return sum(((ZZ(gen_G**i)*P)[1])**2 for i in range(order))
+            return sum(((ZZ(gen_G**i)*P)[0])**2 for i in range(order))
         else:
-            raise NotImplementedError
+            return sum(((ZZ(gen_G**i)*P)[1])**4 for i in range(order))
 
     elif case == 2:
+        gen_G = Zmod(m).unit_gens()[0]**n
+        order = euler_phi(m)//(6*n)
+
         if not Y_coordinates:
-            raise NotImplementedError
+            return sum(((ZZ(gen_G**i)*P)[0])**3 for i in range(order))
         else:
-            raise NotImplementedError
+            return sum(((ZZ(gen_G**i)*P)[1])**2 for i in range(order))
 
 
 
 def find_elliptic_curve(k, K, m_t):
     '''
-    INPUT : 
+    INPUT: 
 
-    a base field k, an extension K, a list integer m_f
+    - ``k`` -- a base field
 
-    OUTPUT : 
+    - ``K`` -- an extension of K of degree n.
+
+    - ``m_t`` -- a list of tuple containing a integer and a set of candidates 
+      for the trace.
+
+    OUTPUT: 
     
-    an elliptic curve over k, an integer m 
+    - An elliptic curve defined over k with the required properties.
 
-    Algorithm :
+    - An integer case, depending on the value of the j-invariant of said 
+      elliptic curve.
+
+    - An integer m statisfying the properties described in isom_elliptic which 
+      we will be using to compute Gaussian periods.
+
+    ..NOTE::
+
+        The case j = 0 or 1728 are not implemented yet. They shall raise 
+        NotImplementedError.
+
+    EXAMPLES:
+    
+    sage: R.<X> = PolynomialRing(GF(5))
+
+    sage: f = R.irreducible_element(19, algorithm = 'random')
+
+    sage: K = GF(5**19, names = 'x', modulus = f)
+
+    sage: m_t = [(229,set([2,37,70,49,200,41,61,194,143,112,113,51,52,54,215,123,157]))]
+
+    sage: find_elliptic(GF(5), K, m_t)
+
+    (Elliptic Curve defined by y^2 = x^3 + x + 2 over Finite Field of size 5,
+    0,
+    229)
+
+    ALGORITHM:
+
+    TODO : Doc is deprecated, to be redone.
 
     Function that finds an elliptic curve with the required charateristics, 
     those given in the function isom_elliptic.
@@ -259,7 +299,6 @@ def find_elliptic_curve(k, K, m_t):
         # requirements
         if j == 1728 :
 
-            '''
             if t == 0:
                 j_rejected.append(j)
                 continue
@@ -267,24 +306,26 @@ def find_elliptic_curve(k, K, m_t):
             # We need a m with the additionnal property that 4 divides 
             # phi(m)
             for i in range(len(m_t)):
-                if not m_t[i][0]%4:
-                    m = None
+                if euler_phi(m_t[i][0])%4 != 0:
+                    m = 0
                     continue
                 else:
                     m = m_t[i][0]
                     S_t = m_t[i][1] 
 
-            # TODO : A test on m to see if it's a prime power or not
-            L = [(E,t), (E.quadratic_twist(), -t), (E.quartic_twist(?), ?t),
-                    (E.quartic_twist(?), ?t)] 
+            if m is None:
+                raise RuntimeError, 'No suitable m found, something\'s wrong.'
+            
+            L = [(E,t), (E.quadratic_twist(), -t), (E.quartic_twist(), t),
+                    (E.quartic_twist(), t)] 
+
             for EE, tt in L:
                 if (Zmod(m)(tt) in S_t):
                     return EE, 1, m
             
             j_rejected.append(j)
-            '''
 
-            raise NotImplementedError
+            #raise NotImplementedError
                 
         elif j == 0 :
             raise NotImplementedError
@@ -298,7 +339,7 @@ def find_elliptic_curve(k, K, m_t):
             L = [(E,t), (E.quadratic_twist(), -t)]
             for EE,tt in L:
                 if (Zmod(m)(tt) in S_t):
-                    return EE, None, m
+                    return EE, 0, m
 
         # We don't want to work on those curves anymore.
         j_rejected.append(j)
